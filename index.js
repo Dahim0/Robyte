d7c3a1ba89245e8a2229fc048c28e5e80324f5ec
abderrahim athimni <abderrahim.athimni2011@gmail.com>
2023-10-09 11:30:33 -0400

    added a very important and satisfaing thing

--- index.js
+++ index.js
@@ -8,3 +8,16 @@ h1.addEventListener('mouseout', function() {
  h1.classList.remove('hover');
 });
 javascript
+
+javascript
+function loadPage(url) {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            document.getElementById("content").innerHTML = this.responseText;
+        }
+    };
+    xhttp.open("GET", url, true);
+    xhttp.send();
+    return false;
+}
